8f7524e5eb089f69982424c00513584748e873cd
wenxuyi <wenxuyi2012@hotmail.com>
2022-06-13 15:34:30 +0800

    添加css到assets

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root :to => "welcome#login"
   get "welcome/say_hello" => "welcome#login"
   get "welcome/home" => "welcome#home"
+  post "welcome/home" => "welcome#home"
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
