4efbb70089960b6142a260f0118dbbee45eba5aa
wenxuyi <wenxuyi2012@hotmail.com>
2022-06-13 14:57:49 +0800

    main

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
     def say
-    end   
+    end 
+    def home
+    end    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   root :to => "welcome#login"
   get "welcome/say_hello" => "welcome#login"
+  get "welcome/home" => "welcome#home"
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
